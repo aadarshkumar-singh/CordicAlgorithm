a9902634538e0bde9a79e9ae5e89a556ada27bc7
Aadarsh Kumar Singh <aadarshxp@gmail.com>
2020-06-29 02:26:01 +0200

    tested vectors at all quadrants , system verilog code restructure and clean up

--- Task_2/cordicAlgorithm_tb.sv
+++ Task_2/cordicAlgorithm_tb.sv
@@ -5,48 +5,58 @@
 module CORDIC_ALGO_TESTBENCH;
 
   localparam width = 32; //width of x and y
+  localparam Ax = 163007430 * 1; // initial value of x
+  localparam Ay = 163007430 * 0;
 
   // CORDIC
-  reg [width-1:0] Xin, Yin;
-  reg [31:0] angle;
-  reg clk;
-  reg signed [63:0] i;
+  logic signed [width-1:0] Xin, Yin;
+  logic signed [31:0] angle;
+  logic signed [63:0] i;
 
-  wire [width-1:0] COSout, SINout;
-
-  localparam Ax = 163007430 * 1;
-  localparam Ay = 163007430 * 0;
+  logic signed [width-1:0] COSout, SINout;
+  real outputValue_cos ;
+  real outputValue_sin;
+  
+  CORDIC_ALGO TEST_RUN(.cosine(COSout), .sine(SINout), .x_start(Xin), .y_start(Yin), .angle(angle));
+  
+  assign outputValue_cos = (COSout /268435456.0);
+  assign outputValue_sin = (SINout /268435456.0);
 
   initial begin
 
-    //set initial values
-	angle = 32'd140552336;
+/*     //set initial values - 30 degree
+	angle = 32'd140552357;
     Xin = Ax;     
-    Yin = Ay;     
+    Yin = Ay;
+	#20;
+	
+	 //set initial values - 60 degree
+	angle = 32'd281104715;
+    Xin = Ax;     
+    Yin = Ay;
+	#20;
+	
+	//set initial values - 120 degree
+	angle = 32'd562209429;
+    Xin = Ax;     
+    Yin = Ay;
+	#20;
+	
+	//set initial values - 210 degree
+	angle = 32'd983866501;
+    Xin = Ax;     
+    Yin = Ay;
+	#20;
+	 */
+	//set initial values - 300 degree
+	angle = 32'd1405523573;
+    Xin = Ax;     
+    Yin = Ay;
+	#20;
+	
 
-    //set clock
-    clk = 'b0;
-    forever
-    begin
-      #5 clk = !clk;
-    end
-
-    #50
-
-    // Test 1
-    #1000
-	angle = 32'd140552336;	
-
-   #1000
    $write("Simulation has finished");
    $stop;
 
   end
-
-  CORDIC_ALGO TEST_RUN(clk, COSout, SINout, Xin, Yin, angle);
-
-  // Monitor the output
-  initial
-  $monitor($time, , COSout, , SINout, , angle);
-
 endmodule
